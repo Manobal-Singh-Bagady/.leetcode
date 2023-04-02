a868da222dbb5599a243960425a636ce360c3f8e
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-04-02 06:19:19 +0530

    Create 977.squares-of-a-sorted-array.py

---
+++ 977.squares-of-a-sorted-array.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=977 lang=python3
+#
+# [977] Squares of a Sorted Array
+#
+
+# @lc code=start
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        ans = [0] * len(nums)
+        w = len(nums) - 1
+        l = 0
+        r = len(nums) - 1
+        l_square = nums[l] ** 2
+        r_square = nums[r] ** 2
+        while w >= 0:
+            if l_square > r_square:
+                ans[w] = l_square
+                l += 1
+                l_square = nums[l] ** 2
+            else:
+                ans[w] = r_square
+                r -= 1
+                r_square = nums[r] ** 2
+            w -= 1
+        return ans
+# @lc code=end
